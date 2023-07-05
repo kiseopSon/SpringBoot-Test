004dd8591b05615ce723f8b7dfc576e8a4fb480f
icarusWings <30857479+kiseopSon@users.noreply.github.com>
2023-07-05 14:48:46 +0900

    first commit

---
+++ src/main/java/com/jpa/GradleConsoleTestApplication.java
@@ -0,0 +1,17 @@
+package com.jpa;
+
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleConsoleTestApplication {
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		SpringApplication.run(GradleConsoleTestApplication.class, args);
+//		Scanner sc = new Scanner(System.in);
+//		sc.next();
+	}
+
+}
