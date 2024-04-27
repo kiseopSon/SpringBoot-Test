84eb7fe5e12ee95a8e37a7127f78622da94cb095
icarusWings <icarusdoll@naver.com>
2024-04-28 00:59:54 +0900

    init

---
+++ Spring3_0Security/src/main/java/com/example/spring3_0security/Spring30SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring3_0security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring30SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring30SecurityApplication.class, args);
+    }
+
+}
---
+++ Spring3_0Security/src/test/java/com/example/spring3_0security/Spring30SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring3_0security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring30SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
